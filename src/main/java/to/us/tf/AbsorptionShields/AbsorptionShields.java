d78e6ebbd565b9aaed2ba2c73a7aecc7829a32e2
RoboMWM <robomwm@users.noreply.github.com>
2017-06-28 17:11:10 -0700

    ye

---
+++ src/main/java/to/us/tf/AbsorptionShields/AbsorptionShields.java
@@ -0,0 +1,13 @@
+package to.us.tf.AbsorptionShields;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created on 3/18/2017.
+ *
+ * @author RoboMWM
+ */
+public class AbsorptionShields extends JavaPlugin
+{
+
+}
---
+++ src/main/java/to/us/tf/AbsorptionShields/ShieldManager.java
@@ -0,0 +1,28 @@
+package to.us.tf.AbsorptionShields;
+
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created on 3/18/2017.
+ *
+ * @author RoboMWM
+ */
+public class ShieldManager
+{
+    Map<Player, String> player = new HashMap<>();
+    void idk()
+    {
+        new BukkitRunnable()
+        {
+            @Override
+            public void run()
+            {
+                player
+            }
+        }.runTaskTimer()
+    }
+}
