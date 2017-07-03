6ea2da191ba015033a1c1ab74ddc1780e1551897
RoboMWM <robomwm@users.noreply.github.com>
2017-07-02 22:12:07 -0700

    more placeholder effects

--- src/main/java/to/us/tf/absorptionshields/shield/ShieldRegeneratationTask.java
+++ src/main/java/to/us/tf/absorptionshields/shield/ShieldRegeneratationTask.java
@@ -1,5 +1,6 @@
 package to.us.tf.absorptionshields.shield;
 
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -61,6 +62,11 @@ public class ShieldRegeneratationTask extends BukkitRunnable
                 float amountToRegen = shield.getRegenRate() / (20L / rateToCheck);
                 float missingShield = shield.getMaxShieldStrength() - shieldHealth;
                 //TODO: regeneration sound effect with pitch on missingShield.
+                player.playSound(player.getLocation(), Sound.BLOCK_NOTE_CHIME, 0.5f, 0.5f + (missingShield / (shield.getMaxShieldStrength() / 1.5f)));
+
+                //TODO: sound effect when starting regeneration of fully depleted shield
+                if (shieldHealth <= 0)
+                    player.playSound(player.getLocation(), Sound.BLOCK_END_PORTAL_SPAWN, 0.1f, 2f);
 
                 //Top off if near full
                 if (amountToRegen > missingShield)
