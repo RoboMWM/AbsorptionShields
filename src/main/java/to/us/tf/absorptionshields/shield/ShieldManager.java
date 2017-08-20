0dc65bfadfd5c0039a958382ddebb42c88e95925
RoboMWM <robomwm@users.noreply.github.com>
2017-08-20 00:20:58 -0700

    Move damage handler to monitor priority
    Some plugins rely on the damage value - after all, we do indeed damage
    the player (albeit we do it manually to avoid issues with armor
    resistance and the like from getting in the way), but if we set the
    damage to 0, any plugins "monitoring" this won't receive this
    information.
    
    Ideally, if md_5 could make a decision with the DamageModifiers API, I
    could then potentially proceed to fix this altogether if the decision is
    to keep it...

--- src/main/java/to/us/tf/absorptionshields/shield/ShieldManager.java
+++ src/main/java/to/us/tf/absorptionshields/shield/ShieldManager.java
@@ -75,7 +75,7 @@ public class ShieldManager implements Listener
         event.getPlayer().removeMetadata("AS_SHIELD", instance);
     }
 
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     private void onPlayerDamaged(EntityDamageEvent event)
     {
         if (event.getEntityType() != EntityType.PLAYER)
